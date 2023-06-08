5b2e89dfaa7a13ac599c307aba80d568ba58d6fd
이민제 <Ol.MinJe@gmail.com>
2023-06-08 10:16:05 +0900

    [IMJ] 작업중 - 알고리즘 컴포넌트 추가 - 알고리즘 컴포넌트 전용 폴더 추가 - css 일부 수정 (.conatiner_inner 적용하기) - App.js 링크에 컴포넌트 연결

---
+++ front_end/src/components/page/AlgorithmsChild.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AlgorithmsMenu from '../Layout/Algorithms/AlgorithmsMenu';
+
+function AlgorithmsChild() {
+  return (
+    <div id='Algorithms' className='container_inner'>
+        <ul className="menu"><AlgorithmsMenu /></ul>
+        AlgorithmsChild
+    </div>
+  )
+}
+
+export default AlgorithmsChild;
