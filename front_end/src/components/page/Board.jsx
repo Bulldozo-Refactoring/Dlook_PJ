6c29e5c3893ab274a96c68d07ced4cee2f12a57e
loadraw <doloadhw1407@gmail.com>
2023-06-08 14:45:58 +0900

    [hwang] board 생성 및 list컴포넌트 추가

--- front_end/src/components/page/Board.jsx
+++ front_end/src/components/page/Board.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import List from '../Layout/List'
 
 function board() {
   return (
-    <div>board</div>
-  )
+  <section className="notice">
+      <div className="page-title">
+          <div className="container">
+              <h3>공지사항</h3>
+          </div>
+      </div>
+
+      <List />
+      
+      {/* -- board seach area -- */}
+      <div id="board-search">
+          <div className="container">
+              <div className="search-window">
+                  <form action="">
+                      <div className="search-wrap">
+                          <label for="search" className="blind">공지사항 내용 검색</label>
+                          <input id="search" type="search" name="" placeholder="검색어를 입력해주세요." value=""/>
+                          <button type="submit" className="btn btn-dark">검색</button>
+                      </div>
+                  </form>
+              </div>
+          </div>
+      </div>
+
+  </section>
+  );
 }
 
-export default board
+export default board;
