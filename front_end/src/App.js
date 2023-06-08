439d2c570b65091d9a42abd1a8d90e41f2c04686
이민제 <Ol.MinJe@gmail.com>
2023-06-08 20:59:46 +0900

    [lmj] router v6 기준으로 링크 수정 - 알고리즘 파일 정리 - 알고리즘 메뉴 작업중

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,37 +1,45 @@
-import { useRoutes } from 'react-router-dom';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import MainPage from './components/page/Main';
 import Board from './components/page/Board';
 import LogIn from './components/auth/Login';
 import SignUp from './components/auth/SignUp';
 import SignUpResult from './components/auth/SignUpResult';
-import AlgorithmsStep from './components/page/AlgorithmsStep';
-import AlgorithmsChild from './components/page/AlgorithmsChild';
-import AlgorithmsType from './components/page/AlgorithmsType';
-import AlgorithmsWrong from './components/page/AlgorithmsWrong';
-import AlgorithmsRank from './components/page/AlgorithmsRank';
+import Algorithms from './components/page/Algorithms';
+import Step from './components/Layout/Algorithms/Step';
+import Child from './components/Layout/Algorithms/Child';
+import Type from './components/Layout/Algorithms/Type';
+import Wrong from './components/Layout/Algorithms/Wrong';
+import Rank from './components/Layout/Algorithms/Rank';
 
-const App = () => {
-  const element = useRoutes([
-    {
-      path: '/',
-      element: <Layout />,
-      children: [
-        { index: true, element: <MainPage /> },
-        { path: 'login', element: <LogIn /> },
-        { path: 'signUp', element: <SignUp /> },
-        { path: 'signupresult', element: <SignUpResult /> },
-        { path: 'algorithms/step', element: <AlgorithmsStep /> },
-        { path: 'algorithms/child', element: <AlgorithmsChild /> },
-        { path: 'algorithms/type', element: <AlgorithmsType /> },
-        { path: 'algorithms/wrong', element: <AlgorithmsWrong /> },
-        { path: 'algorithms/rank', element: <AlgorithmsRank /> },
-        { path: 'board', element: <Board /> },
-      ]
-    }
-  ])
 
-  return element;
-}
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { path: '', element: <MainPage /> },
+      { path: 'login', element: <LogIn /> },
+      { path: 'signUp', element: <SignUp /> },
+      { path: 'signupresult', element: <SignUpResult /> },
+      { path: 'board', element: <Board /> },
+      {
+        path: 'algorithms',
+        element: <Algorithms />,
+        children: [
+          { path: 'step', element: <Step /> },
+          { path: 'child', element: <Child /> },
+          { path: 'type', element: <Type /> },
+          { path: 'wrong', element: <Wrong /> },
+          { path: 'rank', element: <Rank /> },
+        ],
+      },
+    ],
+  },
+  
+]);
 
-export default App;
+export default function App() {
+  return <RouterProvider router={router} />;
+};
